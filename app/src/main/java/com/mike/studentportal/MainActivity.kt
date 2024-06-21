162ed2979d9166445e887435860a741ee7bbd4ff
mikesplore <mikepremium8@gmail.com>
2024-06-21 23:09:47 +0300

    Proceeding well

--- app/src/main/java/com/mike/studentportal/MainActivity.kt
+++ app/src/main/java/com/mike/studentportal/MainActivity.kt
@@ -1,33 +1,54 @@
 package com.mike.studentportal
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Announcement
-import androidx.compose.material.icons.automirrored.filled.Chat
+import androidx.compose.material.icons.automirrored.filled.EventNote
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.google.accompanist.pager.*
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import com.mike.studentportal.CommonComponents as CC
 
@@ -43,27 +64,42 @@ class MainActivity : ComponentActivity() {
 
 sealed class Screen(val title: String, val icon: ImageVector) {
     data object Home : Screen("Home", Icons.Filled.Home)
-    data object Chat : Screen("Chat", Icons.AutoMirrored.Filled.Chat)
-    data object Profile : Screen("Profile", Icons.Filled.Person)
-    data object Timetable : Screen("Timetable", Icons.Filled.CalendarToday)
+    data object Event : Screen("Event", Icons.AutoMirrored.Filled.EventNote)
+    data object Assignments : Screen("Assignments", Icons.Filled.CalendarToday)
     data object Announcements : Screen("Announcements", Icons.AutoMirrored.Filled.Announcement)
-    data object Settings : Screen("Settings", Icons.Filled.Settings)
 }
 
 @OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class, ExperimentalSnapperApi::class)
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
     val pagerState = rememberPagerState()
     val coroutineScope = rememberCoroutineScope()
     val screens = listOf(
         Screen.Home,
-        Screen.Chat,
-        Screen.Profile,
-        Screen.Timetable,
+        Screen.Event,
+        Screen.Assignments,
         Screen.Announcements,
-        Screen.Settings
     )
+    val navController = rememberNavController()
 
+    NavHost(navController, startDestination = "dashboard") {
+        composable("login") { LoginScreen(navController, context) }
+        composable("dashboard") {Dashboard(navController,pagerState,coroutineScope,screens,context)}
+        composable("moredetails") { MoreDetails(context, navController) }
+    }
+}
+
+
+@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun Dashboard(
+    navController: NavController,
+    pagerState: PagerState,
+    coroutineScope: CoroutineScope,
+    screens: List<Screen>,
+    context: Context
+) {
     Scaffold(topBar = {
         TopAppBar(
             title = {
@@ -74,27 +110,25 @@ fun MainScreen() {
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text("Hello, Michael", style = CC.descriptionTextStyle, fontSize = 20.sp)
+                    Text(
+                        "Hello, Michael",
+                        style = CC.descriptionTextStyle(context),
+                        fontSize = 20.sp
+                    )
                     IconButton(onClick = { /*TODO*/ }) {
                         Box(
-                            modifier = Modifier.size(50.dp),
-                            contentAlignment = Alignment.Center
+                            modifier = Modifier.size(50.dp), contentAlignment = Alignment.Center
                         ) {
                             Icon(
                                 imageVector = Icons.Filled.AccountCircle,
                                 contentDescription = "Settings",
                                 tint = Color.White,
                                 modifier = Modifier.fillMaxSize()
-
                             )
                         }
                     }
-
                 }
-
-            },
-
-            colors = TopAppBarDefaults.topAppBarColors(
+            }, colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = CC.primary
             )
         )
@@ -109,27 +143,19 @@ fun MainScreen() {
                     .padding(horizontal = 16.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                val screens = listOf(
-                    Screen.Home,
-                    Screen.Chat,
-                    Screen.Timetable,
-                    Screen.Announcements,
-                    Screen.Profile,
-                    Screen.Settings
-                )
                 screens.forEachIndexed { index, screen ->
                     Box(
                         modifier = Modifier.background(
-                                if (pagerState.currentPage == index) CC.style else Color.Transparent,
-                                RoundedCornerShape(10.dp)
-                            )
+                            if (pagerState.currentPage == index) CC.style else Color.Transparent,
+                            RoundedCornerShape(10.dp)
+                        )
                     ) {
-                        Icon(imageVector = screen.icon, // Use the icon property
+                        Icon(imageVector = screen.icon,
                             contentDescription = screen.title,
                             tint = if (pagerState.currentPage == index) CC.secondary else CC.tertiary,
                             modifier = Modifier
                                 .padding(5.dp)
-                                .size(24.dp) // Adjust icon size as needed
+                                .size(34.dp)
                                 .clickable {
                                     coroutineScope.launch {
                                         pagerState.animateScrollToPage(index)
@@ -144,88 +170,16 @@ fun MainScreen() {
             state = pagerState, count = screens.size, modifier = Modifier.padding(innerPadding)
         ) { page ->
             when (screens[page]) {
-                Screen.Home -> HomeScreen()
-                Screen.Chat -> ChatScreen()
-                Screen.Timetable -> TimetableScreen()
-                Screen.Announcements -> AnnouncementsScreen()
-                Screen.Profile -> ProfileScreen()
-                Screen.Settings -> SettingsScreen()
+                Screen.Home -> HomeScreen(context, navController)
+                Screen.Event -> EventScreen(navController, context)
+                Screen.Assignments -> AssignmentScreen(navController, context)
+                Screen.Announcements -> AnnouncementsScreen(navController, context)
+
             }
         }
     }
 }
 
-
-@Composable
-fun SettingsScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFD3E0EA)),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Settings Screen", style = TextStyle(fontSize = 24.sp, color = Color.Black)
-        )
-    }
-}
-
-@Composable
-fun AnnouncementsScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFD3E0EA)),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Announcements Screen", style = TextStyle(fontSize = 24.sp, color = Color.Black)
-        )
-    }
-}
-
-
-@Composable
-fun ChatScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFD3E0EA)),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Chat Screen", style = TextStyle(fontSize = 24.sp, color = Color.Black)
-        )
-    }
-}
-
-@Composable
-fun ProfileScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFF3D1D1)),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Profile Screen", style = TextStyle(fontSize = 24.sp, color = Color.Black)
-        )
-    }
-}
-
-@Composable
-fun TimetableScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFC1E1C1)),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Timetable Screen", style = TextStyle(fontSize = 24.sp, color = Color.Black)
-        )
-    }
-}
 
 @Preview
 @Composable
