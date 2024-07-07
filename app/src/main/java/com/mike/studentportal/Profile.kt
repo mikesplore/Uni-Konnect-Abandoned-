1d66ba5c0c5151f3029c35a35285d6be7a2f5b86
mikesplore <mikepremium8@gmail.com>
2024-07-07 11:27:50 +0300

    New UI

--- app/src/main/java/com/mike/studentportal/Profile.kt
+++ app/src/main/java/com/mike/studentportal/Profile.kt
@@ -5,7 +5,9 @@ import android.net.Uri
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,11 +16,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Female
+import androidx.compose.material.icons.filled.Male
+import androidx.compose.material.icons.outlined.Female
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -28,6 +34,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -40,7 +48,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -97,6 +107,10 @@ fun ProfileScreen(navController: NavController, context: Context){
             }
             Spacer(modifier = Modifier.height(10.dp))
             DisplayImage(context)
+            Spacer(modifier = Modifier.height(20.dp))
+            ProfileDetails(navController, context)
+            Spacer(modifier = Modifier.height(20.dp))
+            GenderRow(context)
 
 
         }
@@ -164,21 +178,23 @@ fun DisplayImage(context: Context) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if (selectedImageUri != null) {
-            AsyncImage(
-                model = selectedImageUri,
-                contentDescription = "Profile Picture",
-                modifier = Modifier
-                    .size(130.dp)
-                    .clip(CircleShape)
-            )
-        } else if (user?.photoUrl != null) {
+        if (user?.photoUrl != null && user.photoUrl.toString().isNotEmpty()) {
             AsyncImage(
                 model = user.photoUrl,
                 contentDescription = "Profile Picture",
                 modifier = Modifier
                     .size(130.dp)
-                    .clip(CircleShape)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+        } else if (selectedImageUri != null && signInMethod == "password") {
+            AsyncImage(
+                model = selectedImageUri,
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(130.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
             )
         } else {
             Icon(
@@ -194,7 +210,7 @@ fun DisplayImage(context: Context) {
         Spacer(modifier = Modifier.height(20.dp))
 
         Button(
-            onClick = { showImagePicker = true },
+            onClick = { launcher.launch("image/*") },
             shape = RoundedCornerShape(10.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = GlobalColors.secondaryColor
@@ -214,8 +230,143 @@ fun DisplayImage(context: Context) {
     }
 }
 
+
 @Composable
 fun ProfileDetails(navController: NavController, context: Context){
-    Row(modifier  = Modifier) {  }
+    val auth= FirebaseAuth.getInstance()
+    val user = auth.currentUser
+    var currentUser by remember { mutableStateOf(User()) }
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+
+    LaunchedEffect(user?.email) {
+        user?.email?.let {
+            fetchUserDataByEmail(it) { fetchedUser ->
+                fetchedUser?.let {
+                    currentUser = it
+                    firstName = it.firstName
+                    lastName = it.lastName
+                }
+            }
+        }
+    }
+
+    Spacer(modifier = Modifier.height(10.dp))
+    MyDetails(
+        title = "First Name",
+        value = firstName,
+        onValueChange = { firstName = it },
+        context = context
+    )
+    Spacer(modifier = Modifier.height(10.dp))
+    MyDetails(
+        title = "Last Name",
+        value = lastName,
+        onValueChange = { lastName = it },
+        context = context
+    )
+    Spacer(modifier = Modifier.height(10.dp))
+    MyDetails(
+        title = "Email",
+        value = user?.email ?: "",
+        onValueChange = {},
+        context = context
+    )
+    Spacer(modifier = Modifier.height(10.dp))
+    MyDetails(
+        title = "Phone Number",
+        value = user?.phoneNumber ?: "",
+        onValueChange = {},
+        context
+    )
+
 }
 
+@Composable
+fun MyDetails(title: String, value: String, onValueChange: (String) -> Unit, context: Context, fontSize: TextUnit = 18.sp){
+    Row(modifier = Modifier
+        .height(60.dp)
+        .fillMaxWidth(.8f),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(title, style = CC.descriptionTextStyle(context))
+        TextField(
+            value = value,
+            textStyle = CC.titleTextStyle(context).copy(fontSize = fontSize),
+            onValueChange = onValueChange, // Pass the callback directly
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = GlobalColors.tertiaryColor,
+                focusedIndicatorColor = GlobalColors.tertiaryColor,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                focusedTextColor = GlobalColors.textColor,
+                unfocusedTextColor = GlobalColors.textColor,
+            ),
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .height(60.dp)
+        )
+    }
+}
+
+@Composable
+fun GenderRow(context: Context) {
+    var selectedMale by remember { mutableStateOf(false) }
+    var selectedFemale by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .height(70.dp)
+            .fillMaxWidth(0.8f),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text("Gender", style = CC.descriptionTextStyle(context))
+        Spacer(modifier = Modifier.width(10.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(1f),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            IconButton(
+                onClick = {
+                    selectedMale = true
+                    selectedFemale = false
+                },
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(if (selectedMale) GlobalColors.extraColor1 else GlobalColors.secondaryColor, CircleShape)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Male,
+                    contentDescription = "Male",
+                    tint = if (selectedMale) GlobalColors.extraColor2 else GlobalColors.extraColor1,
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(30.dp))
+            IconButton(
+                onClick = {
+                    selectedFemale = true
+                    selectedMale = false
+                },
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(if (selectedFemale) GlobalColors.extraColor1 else GlobalColors.secondaryColor, CircleShape)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Female,
+                    contentDescription = "Female",
+                    tint = if (selectedFemale) GlobalColors.extraColor2 else GlobalColors.extraColor1,
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DangerZone(context: Context){
+    Row {
+        Text("Danger Zone", style = CC.titleTextStyle(context))
+    }
+}
