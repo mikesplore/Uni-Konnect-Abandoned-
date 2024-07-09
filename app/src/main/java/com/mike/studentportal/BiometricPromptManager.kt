f40eb96c9be3eca3fb9bd3ac222e3132a3af7a2b
mikesplore <mikepremium8@gmail.com>
2024-07-09 11:00:38 +0300

    New UI

--- app/src/main/java/com/mike/studentportal/BiometricPromptManager.kt
+++ app/src/main/java/com/mike/studentportal/BiometricPromptManager.kt
@@ -3,79 +3,60 @@ package com.mike.studentportal
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
-import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
-import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.biometric.BiometricPrompt.PromptInfo
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.receiveAsFlow
 
 class BiometricPromptManager(
     private val activity: AppCompatActivity
 ) {
-    private val resultChannel = Channel<BiometricResult>()
-    val promptResult = resultChannel.receiveAsFlow()
-
     fun showBiometricPrompt(
         title: String,
-        description: String
+        description: String,
+        onResult: (Boolean) -> Unit
     ) {
-
-
         val manager = BiometricManager.from(activity)
-        val authenticators = if (Build.VERSION.SDK_INT >= 30) {
-            BIOMETRIC_STRONG or DEVICE_CREDENTIAL
-        } else
-            BIOMETRIC_STRONG
+        val authenticators = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+        } else {
+            BiometricManager.Authenticators.BIOMETRIC_STRONG
+        }
 
         val promptInfo = PromptInfo.Builder()
             .setTitle(title)
             .setDescription(description)
             .setAllowedAuthenticators(authenticators)
             .setConfirmationRequired(false)
+            .build()
 
-        when(manager.canAuthenticate(authenticators)){
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
-                resultChannel.trySend(BiometricResult.HardwareUnavailable)
-                return
-            }
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
-                resultChannel.trySend(BiometricResult.AuthenticationNotSet)
-                return
-            }
+        when (manager.canAuthenticate(authenticators)) {
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE,
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED,
             BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
-                resultChannel.trySend(BiometricResult.FeatureUnavailable)
+                onResult(false)
                 return
             }
             else -> Unit
         }
+
         val prompt = BiometricPrompt(
             activity,
-            object:  BiometricPrompt.AuthenticationCallback(){
+            object : BiometricPrompt.AuthenticationCallback() {
                 override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
                     super.onAuthenticationError(errorCode, errString)
-                    resultChannel.trySend(BiometricResult.AuthenticationError(errString.toString()))
+                    onResult(false)
                 }
 
                 override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
                     super.onAuthenticationSucceeded(result)
-                    resultChannel.trySend(BiometricResult.AuthenticationSuccess)
+                    onResult(true)
                 }
 
                 override fun onAuthenticationFailed() {
                     super.onAuthenticationFailed()
-                    resultChannel.trySend(BiometricResult.AuthenticationFailed)
+                    onResult(false)
                 }
             }
         )
-        prompt.authenticate(promptInfo.build())
-    }
-    sealed interface BiometricResult{
-        data object HardwareUnavailable : BiometricResult
-        data object FeatureUnavailable : BiometricResult
-        data class AuthenticationError( val error: String): BiometricResult
-        data object AuthenticationFailed: BiometricResult
-        data object AuthenticationSuccess: BiometricResult
-        data object  AuthenticationNotSet: BiometricResult
+        prompt.authenticate(promptInfo)
     }
 }
