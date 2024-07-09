7ceb3571aa21b5842bfbf920af5bda190ef31f1b
mikesplore <mikepremium8@gmail.com>
2024-07-09 19:47:27 +0300

    New UI

--- app/src/main/java/com/mike/studentportal/ManageCourses.kt
+++ app/src/main/java/com/mike/studentportal/ManageCourses.kt
@@ -1,41 +1,29 @@
 package com.mike.studentportal
 
 import android.content.Context
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,37 +39,64 @@ fun CoursesScreen(navController: NavController, context: Context) {
 
     LaunchedEffect(loading) {
         MyDatabase.fetchCourses { fetchedCourses ->
-            courses.clear() // Clear existing courses
-            courses.addAll(fetchedCourses) // Add fetched courses
-            loading = false // Set loading to false after fetching
+            courses.clear()
+            courses.addAll(fetchedCourses)
+            loading = false
         }
     }
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Courses", style = CC.titleTextStyle(context)) },
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate("dashboard") }) {
+                        Icon(
+                            Icons.Default.ArrowBackIosNew, "Back", tint = CC.textColor()
+                        )
+                    }
+                },
                 actions = {
                     IconButton(onClick = { loading = true }) {
                         Icon(
-                            Icons.Default.Refresh, "refresh", tint = GlobalColors.textColor
+                            Icons.Default.Refresh, "refresh", tint = CC.textColor()
                         )
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = GlobalColors.primaryColor,
-                    titleContentColor = GlobalColors.textColor
+                    containerColor = CC.primary(),
+                    titleContentColor = CC.textColor()
                 )
             )
         },
-        containerColor = GlobalColors.primaryColor
+        containerColor = CC.primary()
     ) {
         Column(
             modifier = Modifier
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
-                .background(GlobalColors.primaryColor)
-                .padding(it)
+                .background(CC.primary())
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Row(
+                modifier = Modifier
+                    .height(100.dp)
+                    .fillMaxWidth(0.9f)
+            ) {
+                Text(
+                    "Courses",
+                    style = CC.titleTextStyle(context),
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                "Select a course to view its details", style = CC.descriptionTextStyle(context)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
             if (loading) {
                 Column(
                     modifier = Modifier.fillMaxSize(),
@@ -89,7 +104,7 @@ fun CoursesScreen(navController: NavController, context: Context) {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     CircularProgressIndicator(
-                        color = GlobalColors.secondaryColor, trackColor = GlobalColors.textColor
+                        color = CC.secondary(), trackColor = CC.textColor()
                     )
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(
@@ -98,44 +113,7 @@ fun CoursesScreen(navController: NavController, context: Context) {
                 }
             } else {
                 courses.forEach { course ->
-                    Row(
-                        modifier = Modifier
-                            .background(GlobalColors.secondaryColor, RoundedCornerShape(16.dp))
-                            .border(
-                                width = 1.dp, color = GlobalColors.secondaryColor, shape = RoundedCornerShape(10.dp)
-                            )
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Column(
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            Text(
-                                text = course.courseCode,
-                                style = CC.descriptionTextStyle(context),
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = course.courseName,
-                                style = CC.descriptionTextStyle(context),
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis
-                            )
-                        }
-                        IconButton(onClick = {
-                            CourseName.name.value = course.courseName
-                            navController.navigate("course/${course.courseCode}")
-                        }) {
-                            Icon(
-                                Icons.AutoMirrored.Filled.ArrowForwardIos,
-                                contentDescription = "View Course",
-                                tint = GlobalColors.textColor
-                            )
-                        }
-                    }
+                    CourseCard(course = course, navController = navController, context = context)
                     Spacer(modifier = Modifier.height(8.dp))
                 }
             }
@@ -147,4 +125,92 @@ fun CoursesScreen(navController: NavController, context: Context) {
 @Composable
 fun CoursesScreenPreview() {
     CoursesScreen(rememberNavController(), LocalContext.current)
+}
+
+@Composable
+fun CourseCard(course: Course, navController: NavController, context: Context) {
+    var isFlipped by remember { mutableStateOf(false) }
+    val rotation by animateFloatAsState(
+        targetValue = if (isFlipped) 180f else 0f,
+        animationSpec = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
+        label = ""
+    )
+
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable { isFlipped = !isFlipped }
+            .graphicsLayer {
+                rotationY = rotation
+                cameraDistance = 12 * density
+            }
+            .clip(RoundedCornerShape(12.dp))
+            .background(CC.extraColor2())
+    ) {
+        if (rotation <= 90f) {
+            FrontCardContent(course.courseName, navController, course.courseCode, context)
+        } else {
+            BackCardContent(course.courseCode, course.visits.toString(), context)
+        }
+    }
+}
+
+@Composable
+fun FrontCardContent(courseTitle: String, navController: NavController, courseCode: String, context: Context) {
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = courseTitle,
+                style = CC.titleTextStyle(context).copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = "View course materials",
+                    style = CC.descriptionTextStyle(context).copy(fontSize = 16.sp)
+                )
+                IconButton(onClick = { navController.navigate("course/$courseCode") }) {
+                    Icon(Icons.Default.ArrowForward, contentDescription = "View Course")
+                }
+            }
+
+
+        }
+
+
+}
+
+@Composable
+fun BackCardContent(courseCode: String, visits: String, context: Context) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .graphicsLayer {
+                rotationY = 180f
+            }
+    ) {
+        Text(
+            text = "Course Code: $courseCode",
+            style = CC.titleTextStyle(context).copy(fontWeight = FontWeight.Bold, fontSize = 20.sp),
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Visits: $visits",
+            style = CC.descriptionTextStyle(context).copy(fontSize = 16.sp)
+        )
+    }
 }
