d4bf96738fbbdc3bdb5dfb74a061b1fe91582283
mikesplore <mikepremium8@gmail.com>
2024-07-10 16:16:09 +0300

    Renaming to Uni Konnect

--- app/src/androidTest/java/com/mike/unikonnect/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mike/unikonnect/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.mike.studentportal
+package com.mike.unikonnect
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mike.studentportal", appContext.packageName)
+        assertEquals("com.mike.unikonnect", appContext.packageName)
     }
 }
