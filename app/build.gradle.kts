a86faaaa5f8dd378d684b8069ecfced657627e62
mikesplore <mikepremium8@gmail.com>
2024-07-12 01:01:59 +0300

    Renaming to Uni Konnect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.2.5"
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
