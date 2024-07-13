7b3771af5567a9d34d8b8aa33cc886b4108e0f3e
mikesplore <mikepremium8@gmail.com>
2024-07-13 16:17:10 +0300

    new way of installing updates

--- app/src/main/java/com/mike/unikonnect/GithubAuth.kt
+++ app/src/main/java/com/mike/unikonnect/GithubAuth.kt
@@ -11,7 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -35,47 +39,49 @@ fun GitAuth(
     val activity = LocalContext.current as Activity
     val provider = OAuthProvider.newBuilder("github.com")
     var isLoading by remember { mutableStateOf(false) } // State to track loading
+    var success by remember { mutableStateOf(false) }
 
-    Box(
-        modifier = Modifier
-            .clickable {
-                isLoading = true
-                firebaseAuth
-                    .startActivityForSignInWithProvider(activity, provider.build())
-                    .addOnSuccessListener {
-                        isLoading = false // Stop loading on success
-                        onSignInSuccess()
+    Box(modifier = Modifier
+        .clickable {
+            isLoading = true
+            firebaseAuth
+                .startActivityForSignInWithProvider(activity, provider.build())
+                .addOnSuccessListener {
+                    success = true
+                    isLoading = false // Stop loading on success
+                    onSignInSuccess()
 
-                    }
-                    .addOnFailureListener {
-                        isLoading = false // Stop loading on failure
-                        onSignInFailure(it.message ?: "Unknown error")
-                        Log.e("GithubAuth", "Sign-in failed", it)
-                    }
-            }
-            .border(
-                width = 1.dp,
-                color = CC.textColor(),
-                shape = RoundedCornerShape(10.dp)
-            )
-            .background(CC.secondary(), shape = RoundedCornerShape(10.dp))
-            .height(60.dp)
-            .width(130.dp),
-        contentAlignment = Alignment.Center
-    ) {
+                }
+                .addOnFailureListener {
+                    isLoading = false // Stop loading on failure
+                    onSignInFailure(it.message ?: "Unknown error")
+                    Log.e("GithubAuth", "Sign-in failed", it)
+                }
+        }
+        .border(
+            width = 1.dp, color = CC.textColor(), shape = RoundedCornerShape(10.dp)
+        )
+        .background(CC.secondary(), shape = RoundedCornerShape(10.dp))
+        .height(60.dp)
+        .width(130.dp),
+        contentAlignment = Alignment.Center) {
         if (isLoading) {
             // Show CircularProgressIndicator when loading
             CircularProgressIndicator(
-                modifier = Modifier.size(30.dp),
-                color = CC.primary(),
-                trackColor = CC.textColor()
+                modifier = Modifier.size(30.dp), color = CC.primary(), trackColor = CC.textColor()
 
+            )
+        } else if (success) {
+            //show a check to indicate successful authentication
+            Icon(
+                Icons.Default.Check, "Success", tint = CC.textColor()
             )
         } else {
             // Show GitHub image when not loading
             Image(
                 painter = painterResource(R.drawable.github),
                 contentDescription = "GitHub",
+                colorFilter = ColorFilter.tint(CC.textColor()),
                 modifier = Modifier.size(50.dp)
             )
         }
